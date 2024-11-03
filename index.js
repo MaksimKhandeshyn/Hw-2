c9ae8d2bc5849bfd2b601df3ce6eaeca7b0cb4ac
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2024-11-03 22:59:00 +0200

    make homework

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+//First
+let celsiy = 20;
+let farengeit = celsiy * 1.8 + 32;
+console.log("Farengeit: " + farengeit);
+
+//Second
+let valueMonth = 31;
+let hours = valueMonth * 24;
+console.log("Hours in this month is: " + hours);
+let minute = 24 * 60 * valueMonth;
+console.log("Minute in this month is: " + minute);
+
+//Third
+let enegry = 100;
+console.log(enegry - 99 + " Low energy");
+let hp = 100;
+console.log(hp - 99 + " Low health");
+
+//Fourth
+let sumBuy = 657;
+let sale = 10;
+let cost = (sumBuy / 100) * 10;
+console.log("Cost with sale is: " + cost);
+
+//Fifth
+let number = 768.565;
+console.log("Answer: " + Math.floor(number));
+
+//Sixth
+let stringFloat = "11.99qwerty";
+console.log("Float number: " + Number.parseFloat(stringFloat));
+
+//Seventh
+let stringNum = "9qwerty";
+console.log("Integet number: " + Number.parseFloat(stringNum));
+
+//Eighth
+let root = 35;
+console.log("Root of this number is: " + Math.sqrt(root));
+
+//Ninth
+let strNum = "58";
+let intNum = 76;
+let changeNum = Number.parseInt(strNum);
+let changeStr = intNum.toString();
+console.log("This is str type but in change: " + changeNum);
+console.log("This is int type but in change: " + intNum);
